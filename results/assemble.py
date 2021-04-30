255d9ed2d1ca5cfaceb1d89843b17cc93d384ae3
Christian Hayes <crh7gs@virginia.edu>
2021-04-30 12:44:50 -0400

    refactored compile_measurements

--- results/assemble.py
+++ results/assemble.py
@@ -135,63 +135,69 @@ def compile_measurements(path, directory, filename, stars=None, overwrite=False,
     # Create a new group for the target directory
     new_group = comp_file.create_group(directory)
 
-    # We need are using a fixed format, so if a star is missing a line for an element
-    # we need to fill it with a null row which is setup here for ease later
+    for element, lines in elem_line_dict.items():
+        data_array = extract_element(path, directory, stars, element, lines)
+        data = new_group.create_dataset(element, data=data_array)
+
+
+def extract_element(path, directory, stars, element, lines):
+
+        # We need are using a fixed format, so if a star is missing a line for an element
+        # we need to fill it with a null row which is setup here for ease later
     nan_row = [np.nan, np.nan, np.nan, 9, np.nan, 9,
                np.nan, 9, np.nan, 9, np.nan, np.nan,
                np.nan, np.nan, np.nan, np.nan]
 
-    for element, lines in elem_line_dict.items():
-        element_comp_list = []
-        fail_dir = 'failure_log'
-        if not os.path.exists(fail_dir):
-            os.makedirs(fail_dir)
-        fail_path = f'./{fail_dir}/'
-        failed_filename = f'{fail_path}{element}_{directory}_bacchus_failed.txt'
-        failed_log = open(failed_filename, 'w')
-        print('# STAR_ID', file=failed_log)
-        for i, star in enumerate(stars):
-            star_elem_file = f'{path}/{directory}/{element}-{star}.abu'
-            row = []
-            row += [star]
-            # if os.path.exists(star_elem_file):
-            try:
-                data = np.genfromtxt(star_elem_file, names=True,
-                                     dtype=None, encoding=None, skip_header=1)
-                col_id = 1
-                for line in lines:
-                    if np.any(data['lambda'] == line):
+    element_comp_list = []
+    fail_dir = 'failure_log'
+    if not os.path.exists(fail_dir):
+        os.makedirs(fail_dir)
+    fail_path = f'./{fail_dir}/'
+    failed_filename = f'{fail_path}{element}_{directory}_bacchus_failed.txt'
+    failed_log = open(failed_filename, 'w')
+    print('# STAR_ID', file=failed_log)
+    for i, star in enumerate(stars):
+        star_elem_file = f'{path}/{directory}/{element}-{star}.abu'
+        row = []
+        row += [star]
+        # if os.path.exists(star_elem_file):
+        try:
+            data = np.genfromtxt(star_elem_file, names=True,
+                                 dtype=None, encoding=None, skip_header=1)
+            col_id = 1
+            for line in lines:
+                if np.any(data['lambda'] == line):
                         # print(np.array(data[data['lambda'] == line][0]))
-                        row += [*data[data['lambda'] == line][0]]
-                    else:
-                        row += nan_row
-            except:
-                print(star, file=failed_log)
-                row += nan_row * len(lines)
-            # else:
-            #    row += [np.nan] * 16 * len(lines)
+                    row += [*data[data['lambda'] == line][0]]
+                else:
+                    row += nan_row
+        except:
+            print(star, file=failed_log)
+            row += nan_row * len(lines)
+        # else:
+        #    row += [np.nan] * 16 * len(lines)
 
-            element_comp_list += [tuple(row)]
+        element_comp_list += [tuple(row)]
 
-        header = ['lambda', 'eqw_obs', 'syn', 'flag_syn', 'eqw',  'flag_eqw', 'int',
-                  'flag_int', 'chi2', 'flag_chi2', 'chi2_val', 'SNR', 'rvcor',
-                  'limit_syn', 'limit_eqw', 'limit_int']
+    header = ['lambda', 'eqw_obs', 'syn', 'flag_syn', 'eqw',  'flag_eqw', 'int',
+              'flag_int', 'chi2', 'flag_chi2', 'chi2_val', 'SNR', 'rvcor',
+              'limit_syn', 'limit_eqw', 'limit_int']
 
-        dtypes = [np.float_, np.float, np.float, np.int8, np.float, np.int8,
-                  np.float_, np.int8, np.float_, np.int8, np.float_, np.float_,
-                  np.float_, np.float_, np.float_, np.float_]
+    dtypes = [np.float_, np.float, np.float, np.int8, np.float, np.int8,
+              np.float_, np.int8, np.float_, np.int8, np.float_, np.float_,
+              np.float_, np.float_, np.float_, np.float_]
 
-        new_header = []
-        new_header += ['STAR_ID']
-        new_dtypes = []
-        new_dtypes += ['S18']
+    new_header = []
+    new_header += ['STAR_ID']
+    new_dtypes = []
+    new_dtypes += ['S18']
 
-        for i in range(len(lines)):
-            for label, dtype in zip(header, dtypes):
-                new_header += [element + '_' + str(i + 1) + '_' + label]
-                new_dtypes += [dtype]
+    for i in range(len(lines)):
+        for label, dtype in zip(header, dtypes):
+            new_header += [element + '_' + str(i + 1) + '_' + label]
+            new_dtypes += [dtype]
 
-        input_dtype = [(value) for value in zip(new_header, new_dtypes)]
-        data_array = np.array(element_comp_list, dtype=input_dtype)
-        #data_array[:, 0] = data_array[:, 0].astype('S')
-        data = new_group.create_dataset(element, data=data_array)
+    input_dtype = [(value) for value in zip(new_header, new_dtypes)]
+    data_array = np.array(element_comp_list, dtype=input_dtype)
+
+    return data_array
